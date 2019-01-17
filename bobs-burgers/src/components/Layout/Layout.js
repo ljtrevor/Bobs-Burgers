9f89fdec4f68f5853b1b0f08d03b71833e805814
ljtrevor <ljtrevor0689@gmail.com>
2019-01-17 14:47:12 -0800

    created side bar, created a toolbar, added a logo, working on making these components more responsive

--- bobs-burgers/src/components/Layout/Layout.js
+++ bobs-burgers/src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 
 const layout = (props) => (
     <Aux>
-        <div>
-            Toolbar , SideDrawer , Backdrop
-        </div>
+        <Toolbar />
+        <SideDrawer />
     
         <main className={classes.Content}>
             {props.children}
